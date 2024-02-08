109f2fb665047e8307af16a62f95636bc8ae6491
Henio Junior <henioajunior@gmail.com>
2024-02-08 14:00:59 -0300

    Criação de um CRUD básico em HotelResource

---
+++ travel-app/src/main/java/org/acme/hotel/HotelResource.java
@@ -0,0 +1,33 @@
+package org.acme.hotel;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("hotel")
+public class HotelResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Hotel> hotels() {
+        return Hotel.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    public Hotel hotelById(@QueryParam("id") long id) {
+        return Hotel.findById(id);
+    }
+
+    @Transactional
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Hotel newHotel(Hotel hotel) {
+        hotel.id = null;
+        hotel.persist();
+        return hotel;
+    }
+}
