614cdfdbd2220798edddb6d51bed7000c5ace3a0
Henio Junior <henioajunior@gmail.com>
2024-02-19 16:57:59 -0300

    Criação do projeto travelorder-app

---
+++ travelorder-app/src/main/java/org/acme/travelorder/FlightService.java
@@ -0,0 +1,25 @@
+package org.acme.travelorder;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(baseUri = "http://localhost:8081/flight")
+public interface FlightService {
+
+    @GET
+    @Path("findById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight flightById(@QueryParam("id") long id);
+
+    @GET
+    @Path("findByTravelOrderId")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Flight newFlight(Flight flight);
+}
---
+++ travelorder-app/src/main/java/org/acme/travelorder/HotelService.java
@@ -0,0 +1,23 @@
+package org.acme.travelorder;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(baseUri = "http://localhost:8082/hotel")
+public interface HotelService {
+
+    @GET
+    @Path("findByTravelOrderId")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hotel findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
+
+    @GET
+    @Path("findById")
+    public Hotel hotelById(@QueryParam("id") long id);
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Hotel newHotel(Hotel hotel);
+}
